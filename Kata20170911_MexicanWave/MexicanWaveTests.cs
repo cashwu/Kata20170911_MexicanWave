9b0ef2b46dacd4caecffaf9142a32dc2d938245f
cash <cash@cashwu.com>
2017-09-11 21:19:07 +0800

    input empty string

--- Kata20170911_MexicanWave/MexicanWaveTests.cs
+++ Kata20170911_MexicanWave/MexicanWaveTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170911_MexicanWave
@@ -7,8 +9,24 @@ namespace Kata20170911_MexicanWave
     public class MexicanWaveTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_empyt_should_return_empty_list()
         {
+            StringWaveShouldBe(new List<string>(), "");
+        }
+
+        private static void StringWaveShouldBe(List<string> expected, string str)
+        {
+            var kata = new Kata();
+            var actual = kata.wave(str);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public List<string> wave(string str)
+        {
+            return new List<string>();
         }
     }
 }
